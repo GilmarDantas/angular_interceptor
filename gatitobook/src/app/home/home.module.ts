8bddeca8d0438bff4a7d74038b3976ce4d36e98f
GilmarDantas <skypegilmar@gmail.com>
2023-02-14 15:51:53 -0400

    14/02

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -14,9 +15,8 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MensagemModule,
-    ReactiveFormsModule,
+    SharedModule
   ],
   exports: [HomeComponent],
 })
-export class HomeModule {}
+export class HomeModule { }
